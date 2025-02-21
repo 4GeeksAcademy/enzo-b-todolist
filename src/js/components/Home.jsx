4359335d8b7b8bec5bf7a8b33dd1e99cb9e65b75
enzobarrera <154301070+enzobarrera@users.noreply.github.com>
2025-02-21 17:34:25 +0000

    agregar tarea

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,33 @@
-import React from "react";
+import React,{useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [lista, setLista] = useState(["Make the bed", "Wash my hands", "Eat"])
+	const [tarea, setTarea] = useState("")
+
+	const agregarTarea = (e) =>{
+		e.preventDefault()
+		console.log(tarea)
+		setLista([...lista, tarea])
+		setTarea("")
+	}
 	return (
-		<div className="text-center">
+		<div className="text-center container">
             
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="text-center mt-5">To do list</h1>
+			<div className="d-flex justify-content-center">
+			<input className="form-control" value={tarea} onChange={(e)=> setTarea(e.target.value)}/>
+			<button className="btn btn-success" onClick={(e)=> agregarTarea(e)}>Agregar tarea</button>
+			</div>
+			<ul className="list-group border-secondary mt-3">
+				{lista.map((tarea,index) =>(
+					<li key={index} className="list-group-item border border-secondary">{tarea}</li>
+				))}
+			</ul>
 		</div>
 	);
 };
